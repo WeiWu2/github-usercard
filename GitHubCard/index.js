df72168b739a22b055152224a959c9802bd0152d
Bansa Laura <weidiwu207@gmail.com>
2021-01-14 21:54:35 -0500

    imported axios

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,8 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+import axios from "axios";
+console.log(axios.get('https://api.github.com/users/WeiWu2'));
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -16,7 +18,7 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
-function
+
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
     follow this link in your browser https://api.github.com/users/<Your github name>/followers,
@@ -28,8 +30,17 @@ function
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+ const followersArray = [];
 
+ function cardMaker(obj){
+   const card = document.createElement('div');
+
+
+
+
+
+   return card;
+ }
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
